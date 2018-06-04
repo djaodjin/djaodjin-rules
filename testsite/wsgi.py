b4f825529022222ff7cb1ef6639363c02d3f1837
Sebastien Mirolo <smirolo@djaodjin.com>
2018-06-04 05:05:34 -0700

    adds coverage report

--- testsite/wsgi.py
+++ testsite/wsgi.py
@@ -1,19 +1,34 @@
 """
 WSGI config for testsite project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-import os
+import os, signal
+
+#pylint: disable=invalid-name
+
+def save_coverage():
+    sys.stderr.write("saving coverage\n")
+    cov.stop()
+    cov.save()
+
+if os.getenv('DJANGO_COVERAGE'):
+    import atexit, sys
+    import coverage
+    cov = coverage.coverage(data_file=os.path.join(os.getenv('DJANGO_COVERAGE'),
+        ".coverage.%d" % os.getpid()))
+    cov.start()
+    atexit.register(save_coverage)
+    try:
+        signal.signal(signal.SIGTERM, save_coverage)
+    except ValueError as e:
+        # trapping signals does not work with manage
+        # trying to do so fails with
+        # ValueError: signal only works in main thread
+        pass
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite.settings")
 
@@ -21,8 +36,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application() #pylint: disable=invalid-name
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+from whitenoise.django import DjangoWhiteNoise
+#pylint: disable=invalid-name
+application = DjangoWhiteNoise(get_wsgi_application())
