437739bbd52f87d938f0befca200cdea69d8eeb8
Sebastien Mirolo <smirolo@djaodjin.com>
2020-10-24 22:47:34 -0700

    hotfix for commit 78cf4746

--- rules/settings.py
+++ rules/settings.py
@@ -62,7 +62,7 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.utils import inspect
 
-from .compat import is_authenticated, reverse, six
+from .compat import is_authenticated, reverse_lazy, six
 
 
 _SETTINGS = {
