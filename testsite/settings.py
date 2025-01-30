5e3dae20989d2d2786e2b14974567b05ba3e1221
Sebastien Mirolo <smirolo@djaodjin.com>
2025-01-30 08:50:02 -0800

    matches login expected by tests

--- testsite/settings.py
+++ testsite/settings.py
@@ -1,12 +1,18 @@
 # Django settings for testsite project.
 
 import logging, os, re, sys
+from random import choice
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-RUN_DIR = os.getcwd()
+RUN_DIR = os.getenv('RUN_DIR', os.getcwd())
+DB_NAME = os.path.join(RUN_DIR, 'db.sqlite')
+LOG_FILE = os.path.join(RUN_DIR, 'testsite-app.log')
 
-#JS_FRAMEWORK = 'angularjs'
+DEBUG = True
+FEATURES_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 JS_FRAMEWORK = 'vuejs'
+
 
 def load_config(confpath):
     '''
@@ -24,14 +30,17 @@ def load_config(confpath):
                     look = re.match(r'(\w+)\s*=\s*(.*)', line)
                     if look:
                         value = look.group(2) \
-                            % {'LOCALSTATEDIR': BASE_DIR + '/var'}
-                        # Once Django 1.5 introduced ALLOWED_HOSTS (a tuple
-                        # definitely in the site.conf set), we had no choice
-                        # other than using eval. The {} are here to restrict
-                        # the globals and locals context eval has access to.
-                        # pylint: disable=eval-used
-                        setattr(sys.modules[__name__],
-                            look.group(1).upper(), eval(value, {}, {}))
+                            % {'LOCALSTATEDIR': RUN_DIR + '/var'}
+                        try:
+                            # Once Django 1.5 introduced ALLOWED_HOSTS (a tuple
+                            # definitely in the site.conf set), we had no choice
+                            # other than using eval. The {} are here to restrict
+                            # the globals and locals context eval has access to.
+                            # pylint: disable=eval-used
+                            setattr(sys.modules[__name__],
+                                look.group(1).upper(), eval(value, {}, {}))
+                        except Exception:
+                            raise
                 line = conffile.readline()
     else:
         sys.stderr.write('warning: config file %s does not exist.\n' % confpath)
@@ -39,14 +48,9 @@ def load_config(confpath):
 load_config(os.path.join(
     os.getenv('TESTSITE_SETTINGS_LOCATION', RUN_DIR), 'credentials'))
 
-DEBUG = True
-FEATURES_DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ROOT_URLCONF = 'testsite.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'testsite.wsgi.application'
+if not hasattr(sys.modules[__name__], "SECRET_KEY"):
+    SECRET_KEY = "".join([choice(
+        "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^*-_=+") for i in range(50)])
 
 INSTALLED_APPS = (
     'django_extensions',
@@ -65,19 +69,24 @@ INSTALLED_APPS = (
     'testsite'
 )
 
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-DEBUG_TOOLBAR_CONFIG = {
-    'JQUERY_URL': '/static/vendor/jquery.js',
-    'SHOW_COLLAPSED': True,
-    'SHOW_TEMPLATE_CONTEXT': True,
-}
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'testsite.wsgi.application'
+ROOT_URLCONF = 'testsite.urls'
 
-INTERNAL_IPS = ('127.0.0.1', '::1')
+MIDDLEWARE = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(RUN_DIR, 'db.sqlite'),
+        'NAME': DB_NAME,
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
@@ -92,6 +101,31 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
 }
+
+# Static files (CSS, JavaScript, Images)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'testsite', 'media')
+MEDIA_URL = '/media/'
+
+APP_STATIC_ROOT = os.path.join(BASE_DIR, 'testsite', 'static')
+STATIC_URL = '/static/'
+
+if DEBUG:
+    STATIC_ROOT = ''
+    # Additional locations of static files
+    STATICFILES_DIRS = (APP_STATIC_ROOT,)
+else:
+    STATIC_ROOT = APP_STATIC_ROOT
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -114,53 +148,74 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'simple': {
+            'format': 'X X %(levelname)s [%(asctime)s] %(message)s',
+            'datefmt': '%d/%b/%Y:%H:%M:%S %z'
+        },
+    },
+    'handlers': {
+        'log': {
+            'level':'DEBUG',
+            'formatter': 'simple',
+            'class':'logging.StreamHandler',
+        },
+        'db_log': {
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'filters': ['require_debug_true'],
+            'class':'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'rules': {
+            'handlers': ['log'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+#        'django.db.backends': {
+#             'handlers': ['db_log'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#        },
+        'django.request': {
+            'handlers': [],
+            'level': 'ERROR',
+        },
+        # If we don't disable 'django' handlers here, we will get an extra
+        # copy on stderr.
+        'django': {
+            'handlers': [],
+        },
+        # This is the root logger.
+        # The level will only be taken into account if the record is not
+        # propagated from a child logger.
+        #https://docs.python.org/2/library/logging.html#logging.Logger.propagate
+        '': {
+            'handlers': ['log'],
+            'level': 'WARNING'
+        }
+    }
+}
+if logging.getLogger('gunicorn.error').handlers:
+    LOGGING['handlers']['log'].update({
+        'class':'logging.handlers.WatchedFileHandler',
+        'filename': LOG_FILE
+    })
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-MIDDLEWARE = (
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
-)
-
-MIDDLEWARE_CLASSES = MIDDLEWARE
 
 # Templates (Django 1.8+)
 # ----------------------
@@ -195,49 +250,17 @@ LOGIN_REDIRECT_URL = '/proxy/rules/'
 # Allow user to enter month in durationfield
 DURATIONFIELD_ALLOW_MONTHS = True
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'logfile':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'rules': {
-            'handlers': ['logfile'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        }
-    }
-}
-if logging.getLogger('gunicorn.error').handlers:
-    LOGGING['handlers']['logfile'].update({
-        'class':'logging.handlers.WatchedFileHandler',
-        'filename': os.path.join(RUN_DIR, 'testsite-app.log')
-    })
 
+# debug panel
+# -----------
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': '/static/vendor/jquery.js',
+    'SHOW_COLLAPSED': True,
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+
+INTERNAL_IPS = ('127.0.0.1', '::1')
 
 # Session settings
 # ----------------
